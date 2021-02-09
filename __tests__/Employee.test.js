b294996e4aef94f39a0d2de4965c5c33179019e4
jkaho <72959522+jkaho@users.noreply.github.com>
2021-02-09 23:43:29 +1100

    Modify 'Employee.test.js' to add getRole() test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,10 +1,23 @@
+const { expect, it } = require('@jest/globals');
 const Employee = require('../lib/Employee');
+const inquirer = require('inquirer');
+
+const mock = jest.spyOn(inquirer, "prompt");
+mock.mockImplementation(() => new Promise({resolve: () => console.log('success')}));
 
 describe('Employee class', () => {
-    it('getName returns name from inquirer prompt', () => {
-        const num1 = 1;
-        const num2 = 2;
-        const sum = 3;
-        expect(num1 + num2).toEqual(sum);
+    it('getRole returns the string "Employee"', () => {
+        const bob = new Employee();
+        const bobRole = bob.getRole();
+        expect(bobRole).toEqual('Employee');
     });
+
+    // it('getName returns name from inquirer prompt', () => {
+    //     const bob = new Employee();
+    //     // mock.mockImplementation(answer => {answer.name = "bob"});
+    //     // mock.mockImplementation(() => new Promise({resolve: () => console.log('success)}))
+    //     // expect(mock).resolves.toEqual(...);
+    //     expect(bob.getName()).toEqual("bob");
+    //     // test the input assuming inquirer works 
+    // });
 })
