3f46807896196aadf022356d7805a999db751e6f
jkaho <72959522+jkaho@users.noreply.github.com>
2021-02-09 17:49:22 +1100

    Modify 'Employee.js' to add class methods, modify 'Employee.test.js' to test jest functionality

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,10 @@
+const Employee = require('../lib/Employee');
+
+describe('Employee class', () => {
+    it('getName returns name from inquirer prompt', () => {
+        const num1 = 1;
+        const num2 = 2;
+        const sum = 3;
+        expect(num1 + num2).toEqual(sum);
+    });
+})
