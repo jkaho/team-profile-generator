d708f2dff661e03b5e4a17b92fc7eec0b24eda86
jkaho <72959522+jkaho@users.noreply.github.com>
2021-02-10 16:00:38 +1100

    Modify 'Manager.test.js' to add test for number of Manager class properties

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,9 +1,10 @@
-const Manager = require('Manager');
+const { expect } = require('@jest/globals');
+const Manager = require('../lib/Manager');
 
 describe("Manager class", () => {
     it("New Manager instance has 4 properties", () => {
-        const employee = new Employee('Bob', 1, 'bob@work.com');
-        const employeeProperties = Object.keys(employee);
-        expect(employeeProperties.length).toEqual(3);
+        const manager = new Manager('Shirley', 2, 'shirley@work.com', 1);
+        const managerProperties = Object.keys(manager);
+        expect(managerProperties.length).toEqual(4);
     });
 });
