926a62a78598b203c7ffccb3b23b7bbbd7c954a5
jkaho <72959522+jkaho@users.noreply.github.com>
2021-02-10 16:02:25 +1100

    Modify 'Manager.test.js' to add test for getName

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,10 +1,15 @@
 const { expect } = require('@jest/globals');
 const Manager = require('../lib/Manager');
+const manager = new Manager('Shirley', 2, 'shirley@work.com', 1);
 
 describe("Manager class", () => {
     it("New Manager instance has 4 properties", () => {
-        const manager = new Manager('Shirley', 2, 'shirley@work.com', 1);
         const managerProperties = Object.keys(manager);
         expect(managerProperties.length).toEqual(4);
     });
+
+    it("getName returns Manager name", () => {
+        const managerName = manager.getName();
+        expect(managerName).toEqual('Shirley');
+    })
 });
