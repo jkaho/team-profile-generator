7a1bdb06220369d8267f64998e726019b6cc168b
jkaho <72959522+jkaho@users.noreply.github.com>
2021-02-10 23:15:52 +1100

    Modify 'generateHTML.js' to add tags to html head

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -70,6 +70,10 @@ function generateHTML(arr) {
     console.log(`<!DOCTYPE html>
 <html>
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Team Profile</title>
 </head>
 <body>
     ${renderTeam(arr)}
