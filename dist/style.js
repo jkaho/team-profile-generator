813cc7a292c4368a33038f751c0d1fd078fb8c91
jkaho <72959522+jkaho@users.noreply.github.com>
2021-02-12 19:13:10 +1100

    Modify 'style.js' to specify max-width for responsiveCardHeight function

--- dist/style.js
+++ dist/style.js
@@ -1,7 +1,9 @@
 const cardTop = document.querySelectorAll(".card-top");
 
-responsiveCardHeight();
-window.document.addEventListener("resize", responsiveCardHeight);
+if (window.innerWidth <= 768) {
+    responsiveCardHeight();
+    window.document.addEventListener("resize", responsiveCardHeight);
+}
 
 function responsiveCardHeight() {
     for (var i = 0; i < cardTop.length; i++) {
