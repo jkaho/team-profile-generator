606202aa403a48f323a8649ca024c7d0987f255b
jkaho <72959522+jkaho@users.noreply.github.com>
2021-02-12 20:49:19 +1100

    Modify 'generateHTML.js' to add style changes

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,41 +1,53 @@
 // Renders manager information
 function renderManager(manager) {
-    return `<div>
-        <h1>${manager.getName()}</h1>
-        <p>
-            Role: ${manager.getRole()}<br>
-            Employee ID: ${manager.getId()}<br>
-            Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a><br>
-            Office number: ${manager.officeNumber}
-        </p>
+    return `<div class="card manager">
+        <div class="card-top">
+            <h2>${manager.getName()}</h2>
+            <h3><i class="fas fa-user-tie"></i> ${manager.getRole()}</h3>
+        </div>
+        <div class="card-bottom>
+            <p>
+            <i class="far fa-id-card"></i> ID #${manager.getId()}<br>
+            <i class="far fa-envelope"></i> <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a><br>
+            <i class="far fa-building"></i> Office #${manager.officeNumber}
+            </p>
+        </div>
     </div>
 `
 }
 
 // Renders engineer information
 function renderEngineer(engineer) {
-    return `<div>
-        <h1>${engineer.getName()}</h1>
-        <p>
-            Role: ${engineer.getRole()}<br>
-            Employee ID: ${engineer.getId()}<br>
-            Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a><br>            
-            GitHub: <a href="https://github.com/${engineer.github}">${engineer.github}</a>
-        </p>
+    return `<div class="card engineer">
+        <div class="card-top">
+            <h2>${engineer.getName()}</h2>
+            <h3><i class="fas fa-laptop-code"></i> ${engineer.getRole()}</h3>
+        </div>
+        <div class="card-bottom">
+            <p>
+                <i class="far fa-id-card"></i> ID #${engineer.getId()}<br>
+                <i class="far fa-envelope"></i> <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a><br>            
+                <i class="fab fa-github"></i> <a href="https://github.com/${engineer.getGithub()}">${engineer.getGithub()}</a>
+            </p>
+        </div>
     </div>
 `
 }
 
 // Renders intern information
 function renderIntern(intern) {
-    return `<div>
-        <h1>${intern.getName()}</h1>
-        <p>
-            Role: ${intern.getRole()}<br>
-            Employee ID: ${intern.getId()}<br>
-            Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a><br>            
-            School: ${intern.school}
-        </p>
+    return `<div class="card intern">
+        <div class="card-top">
+            <h2>${intern.getName()}</h2>
+            <h3><i class="fas fa-user-graduate"></i> ${intern.getRole()}</h3>
+        </div>
+        <div class="card-bottom">
+            <p>
+                <i class="far fa-id-card"></i> ID #${intern.getId()}<br>
+                <i class="far fa-envelope"></i> <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a><br>    
+                <i class="fas fa-graduation-cap"></i> ${intern.getSchool()}        
+            </p>
+        </div>
     </div>
 `
 }
@@ -73,12 +85,20 @@ function generateHTML(arr) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">    
+    <link
+    rel="stylesheet"
+    href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+    integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
+    crossorigin="anonymous"
+    /> 
     <link rel="stylesheet" href="./profile.html">
     <title>Team Profile</title>
 </head>
 <body>
     <header>
-        <h1>My Team</h1>
+        <h1>OUR <span id="team-span">TEAM</span></h1>
     </header>
     <main>
     ${renderTeam(arr)}
