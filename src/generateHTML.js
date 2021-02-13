4b1e1beeda74eaf13fa3db5ec4f172525e3a4ca6
jkaho <72959522+jkaho@users.noreply.github.com>
2021-02-13 15:36:57 +1100

    Modify 'generateHTML.js' to edit html tag attribute

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -80,7 +80,7 @@ function renderTeam(arr) {
 // Generates team profile HTML page template
 function generateHTML(arr) {
     return `<!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
