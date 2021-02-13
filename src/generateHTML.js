9edb08610c2216fa71de0d0f5f97bfad2a0a51b0
jkaho <72959522+jkaho@users.noreply.github.com>
2021-02-13 15:34:36 +1100

    Modify 'generateHTML.js' to edit html indentation

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,54 +1,54 @@
 // Renders manager information
 function renderManager(manager) {
-    return `<div class="card manager">
-        <div class="card-top">
-            <h2>${manager.getName()}</h2>
-            <h3><i class="fas fa-user-tie"></i> ${manager.getRole()}</h3>
+    return `    <div class="card manager">
+            <div class="card-top">
+                <h2>${manager.getName()}</h2>
+                <h3><i class="fas fa-user-tie"></i> ${manager.getRole()}</h3>
+            </div>
+            <div class="card-bottom">
+                <p>
+                <i class="far fa-id-card"></i> ID #${manager.getId()}<br>
+                <i class="far fa-envelope"></i> <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a><br>
+                <i class="far fa-building"></i> Office #${manager.officeNumber}
+                </p>
+            </div>
         </div>
-        <div class="card-bottom">
-            <p>
-            <i class="far fa-id-card"></i> ID #${manager.getId()}<br>
-            <i class="far fa-envelope"></i> <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a><br>
-            <i class="far fa-building"></i> Office #${manager.officeNumber}
-            </p>
-        </div>
-    </div>
 `
 }
 
 // Renders engineer information
 function renderEngineer(engineer) {
-    return `    <div class="card engineer">
-        <div class="card-top">
-            <h2>${engineer.getName()}</h2>
-            <h3><i class="fas fa-laptop-code"></i> ${engineer.getRole()}</h3>
+    return `        <div class="card engineer">
+            <div class="card-top">
+                <h2>${engineer.getName()}</h2>
+                <h3><i class="fas fa-laptop-code"></i> ${engineer.getRole()}</h3>
+            </div>
+            <div class="card-bottom">
+                <p>
+                    <i class="far fa-id-card"></i> ID #${engineer.getId()}<br>
+                    <i class="far fa-envelope"></i> <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a><br>            
+                    <i class="fab fa-github"></i> <a href="https://github.com/${engineer.getGithub()}" target="_blank">${engineer.getGithub()}</a>
+                </p>
+            </div>
         </div>
-        <div class="card-bottom">
-            <p>
-                <i class="far fa-id-card"></i> ID #${engineer.getId()}<br>
-                <i class="far fa-envelope"></i> <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a><br>            
-                <i class="fab fa-github"></i> <a href="https://github.com/${engineer.getGithub()}" target="_blank">${engineer.getGithub()}</a>
-            </p>
-        </div>
-    </div>
 `
 }
 
 // Renders intern information
 function renderIntern(intern) {
-    return `    <div class="card intern">
-        <div class="card-top">
-            <h2>${intern.getName()}</h2>
-            <h3><i class="fas fa-user-graduate"></i> ${intern.getRole()}</h3>
+    return `        <div class="card intern">
+            <div class="card-top">
+                <h2>${intern.getName()}</h2>
+                <h3><i class="fas fa-user-graduate"></i> ${intern.getRole()}</h3>
+            </div>
+            <div class="card-bottom">
+                <p>
+                    <i class="far fa-id-card"></i> ID #${intern.getId()}<br>
+                    <i class="far fa-envelope"></i> <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a><br>    
+                    <i class="fas fa-graduation-cap"></i> ${intern.getSchool()}        
+                </p>
+            </div>
         </div>
-        <div class="card-bottom">
-            <p>
-                <i class="far fa-id-card"></i> ID #${intern.getId()}<br>
-                <i class="far fa-envelope"></i> <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a><br>    
-                <i class="fas fa-graduation-cap"></i> ${intern.getSchool()}        
-            </p>
-        </div>
-    </div>
 `
 }
 
